32353f0ddb714c1320adfbb4c46b95a947fcb494
Jens Winkel <j.winkel@student.xu-university.de>
2024-04-01 19:08:04 +0200

    added basics for state machine

---
+++ scripts/States/State Machine.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@export var initial_state : State
+
+var current_state : State
+var states : Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.Transitioned.connect(on_child_transition)
+			
+func _process(delta):
+	if current_state.Enter():
+		current_state = initial_state
+		
+		
+func _physics_process(delta):
+	if current_state:
+		current_state.Physics_Update(delta)
+		
+func on_child_transition(state, new_state_name):
+	if state != current_state:
+		return
+	
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		return
+	
+	if current_state:
+		current_state.Exit()
+	new_state.Enter()
+	
+	current_state = new_state
